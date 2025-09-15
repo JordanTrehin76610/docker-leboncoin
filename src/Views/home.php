99e90fe3862be60ab535fd6f6d02af558380e05e
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-09-15 16:54:33 +0200

    Template pour la navbar

--- src/Views/home.php
+++ src/Views/home.php
@@ -14,50 +14,7 @@
 
 <body>
 
-    <navbar>
-        <div class="container text-center">
-            <div class="row pt-2 my-auto">
-                <div class="col-8 text-start mt-2">
-                    <a href="index.php?url=home"><img src="uploads/logo.jpg" alt="Logo Leboncon"></a>
-                </div>
-                <?php if(isset($_SESSION['username'])) { ?>
-                <div class="col-2">
-                    <div class="row">
-                        <a href="index.php?url=logout" class="btn bouton"><button class="boutton"
-                                type="button">Déconnexion</button></a>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <a href="index.php?url=profil" class="text-dark text-decoration-none">
-                        <div class="row">
-                            <i class="bi bi-person icone"></i>
-                        </div>
-                        <div class="row">
-                            <p class="text-dark text-decoration-none">Bonjour <?= $_SESSION['username'] ?> </p>
-                        </div>
-                    </a>
-                </div>
-                <?php } else { ?>
-                <div class="col-2">
-                    <div class="row">
-                        <a href="index.php?url=register" class="btn bouton"><button class="boutton"
-                                type="button">Inscription</button></a>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <a href="index.php?url=login" class="text-dark text-decoration-none">
-                        <div class="row">
-                            <i class="bi bi-person icone"></i>
-                        </div>
-                        <div class="row">
-                            <p class="text-dark text-decoration-none">Se connecter</p>
-                        </div>
-                    </a>
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-    </navbar>
+<?php include_once 'templates/navbar.php'; ?>
 
 
     <hr>
